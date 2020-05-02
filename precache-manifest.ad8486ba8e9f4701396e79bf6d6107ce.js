228596894d83c8c6b8771fa72a6c6b43a64e78ee
Bartosz Trzos <Bartosz.Trzos@ipfdigital.com>
2020-05-02 14:19:19 +0200

    Updates

---
+++ precache-manifest.ad8486ba8e9f4701396e79bf6d6107ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dbb0314616cd86f4bb125ba6cf71e0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13abd80b0ea8c7e98416",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "32b491e4abe2dd8b6328",
+    "url": "/static/js/2.3ef05815.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3ef05815.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13abd80b0ea8c7e98416",
+    "url": "/static/js/main.81e406da.chunk.js"
+  },
+  {
+    "revision": "2b1966e609115edb2dc0",
+    "url": "/static/js/runtime-main.787cdf2b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
